d9b3e468453f32d5b42540c54fc9983e836719b0
vortexdude <nitinnamdeo456@gmail.com>
2023-06-20 16:22:01 +0530

    add the doc for dcim service

--- resources/dcim.py
+++ resources/dcim.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from .docs.main import dcim_doc
 
 blp = Blueprint("DCIM", __name__, description="DCIM, Auth required")
 
@@ -8,8 +9,7 @@ blp = Blueprint("DCIM", __name__, description="DCIM, Auth required")
 class Dcim(MethodView):
 
     @jwt_required()
+    @blp.doc(**dcim_doc)
     def get(self):
         current_user = get_jwt_identity()
         return {"message": "protected route", "current_user": current_user['fname']}
-        
-        
---
+++ resources/docs/main.py
@@ -0,0 +1,9 @@
+dcim_doc = {
+    'summary': "Get all the sites",
+    'description': "Will return all the sites related to user. so user must me loggedin",
+    'parameters': {'Authorization': {
+                    'name': 'Authorization', 
+                    'in': 'header', 
+                    'description': 'Authorization: Bearer <access_token>', 
+                    'required': 'true'}
+                    }}
