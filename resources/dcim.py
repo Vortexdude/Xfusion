2da1ce861ae005f1abaf8cd66f3c9d1b8ace65dd
vortexdude <nitinnamdeo456@gmail.com>
2023-06-20 16:04:23 +0530

    add docs for the smorest

--- resources/dcim.py
+++ resources/dcim.py
@@ -1,12 +1,14 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from .docs.main import dcim_doc
 
 blp = Blueprint("DCIM", __name__, description="DCIM, Auth required")
 
+
 @blp.route("/dcim")
 class Dcim(MethodView):
-
+    @blp.doc(**dcim_doc)
     @jwt_required()
     def get(self):
         current_user = get_jwt_identity()
---
+++ resources/docs/main.py
@@ -0,0 +1,5 @@
+dcim_doc = {
+       'description': "Return all the sites based upon the user",
+       'summary': 'Get all the site locations',
+       'parameters': {'Authorization': {'name': 'Authorization', 'in': 'header', 'description': 'Authorization: Bearer <access_token>', 'required': 'true'}}
+}
