3abc4e1795472e98988b42cfdc7fe4a62c93dd6b
vortexdude <nitinnamdeo456@gmail.com>
2023-07-21 23:28:18 +0530

    adding paths and logic for handling the image

--- app/api/image_proccessing/blueprint.py
+++ app/api/image_proccessing/blueprint.py
@@ -1,29 +1,15 @@
+from flask import send_from_directory
 from flask.views import MethodView
 from flask_smorest import Blueprint
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from .controller import ImageProccessing
 from uuid import uuid4
-from .schema import MultipartFileSchema
-from werkzeug.utils import secure_filename
 import os
+from werkzeug.utils import secure_filename
+from .controller import ImageProccessing
+from .schema import MultipartFileSchema
+from app.common.paths import dir
 
 blp = Blueprint("Image_Proccessing", __name__, description="Proccessing Image using openCV")
-
-class dir:
-    def __init__(self):
-        self.pwd = os.getcwd()
-
-    @property
-    def output(self):
-        dir = f"{self.pwd}/output"
-        os.makedirs(f"{self.pwd}/output", exist_ok=True)
-        return dir
-
-    @property
-    def proccesed(self):
-        dir = f"{self.pwd}/proccessed"
-        os.makedirs(f"{self.pwd}/proccessed", exist_ok=True)
-        return dir
 
 dir = dir()
 
@@ -33,9 +19,13 @@ class ImageProccessingRoute(MethodView):
     @jwt_required()
     @blp.arguments(MultipartFileSchema, location='files')
     def post(self, file):
-        base_dir = os.getcwd()
+        if not file:
+            return {'message': 'Please upload file'}
         _file = file['file']
         _file.save(os.path.join(dir.output, secure_filename(_file.filename)))
-        current_user = get_jwt_identity()
-        ImageProccessing.detect_face(f"{dir.output}/{_file.filename}", f"{dir.proccesed}/{uuid4()}.jpg")
-        return {"message": "protected route", "current_user": current_user['fname']}
+        image_name = f"{uuid4()}.jpg"
+        try:
+            ImageProccessing.detect_face(f"{dir.output}/{_file.filename}", f"{dir.proccesed}/{image_name}")
+        except:
+            return {"message": "Face not found"}
+        return send_from_directory(dir.proccesed, image_name)
